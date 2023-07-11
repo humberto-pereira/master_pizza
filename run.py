fa9691dc39fa755cfef8b5fa2e5a39f618dff3fe
Humberto Pereira <humberto_pereira@live.com>
2023-07-11 20:22:14 +0000

    Add pizza ingredients dictionaries

--- run.py
+++ run.py
@@ -15,26 +15,42 @@ SHEET = GSPREAD_CLIENT.open('master_pizza')
 
 class Pizza_Management:
     """
-    management Class
+    Pizzeria management Class
     """
     def __init__(self):
         self.ingredients_kg_price = []
-        self.margherita_dictionary = {
-            'flour' : 0.35,
-            'east' : 0.003,
-            'salt' : 0.007,
-            'sugar' : 0.010,
-            'tomato sauce': 0.5,
-            'cheese' : 0.18,
-            'tomatoes' : 0.03,
-            'oregano' : 0.003
-            }
+        # Dictionaries with key ingredients and respective weight in kg
+        self.ingredients_constant_dict = {'flour': 0.35, 'east': 0.003, 'salt': 0.007, 'sugar': 0.010, 'tomato sauce': 0.5, 'cheese': 0.18}
+        self.margherita_dict = {'tomatoes': 0.03, 'oregano': 0.003}
+
+        self.salami_dict = {'salami': 0.04, 'oregano': 0.003}
+            
+        self.arugula_dict = {'arugula': 0.01, 'parma ham': 0.025, 'oregano': 0.003}
+            
+        self.chicken_dict = {'chicken': 0.04, 'champignon': 0.015, 'oregano': 0.003}
+            
+        self.prosciutto_dict = {'ham': 0.04, 'champignon': 0.015, 'olives': 0.015, 'oregano': 0.003}
+
+        self.caprese_dict = {'tomatoes': 0.03, 'olives': 0.015, 'onion': 0.03, 'oregano': 0.003}
+
+        self.tuna_dict = {'tuna': 0.04, 'onion': 0.03, 'olives': 0.015, 'oregano': 0.003}   
+
+        self.hawaii_dict = {'ham': 0.04, 'pineapple': 0.035}
+
+        self.funghi_dict = {'champignon': 0.015, 'ham': 0.04, 'onion': 0.03, 'oregano': 0.003}
+        
+        self.meat_lovers_dict = {'meat': 0.04, 'onion': 0.03, 'tomatoes': 0.03, 'oregano': 0.003}
+            
 
     def calculate_pizza_cost(self):
-        self.ingredients_kg_price = SHEET.worksheet('ingredients kg price').get_all_values()
+        """
+        Calculate pizza cost: (ingredients kg price) * (ingredients)
+        """
+        self.ingredients_kg_price = SHEET.worksheet('ingredients kg prices').get_all_values()
+        # Transform list into dictionary and transform str to float
         ingredients_kg_price_dict = {key: float(value) for key, value in zip (self.ingredients_kg_price[0], self.ingredients_kg_price[1])}
         return ingredients_kg_price_dict
-        if 
+        
 
     # def calculate_shopping_list_cost(self):
 
