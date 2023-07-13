12dc2e967cddd0564e542c9d4640173d9d88b5f1
Humberto Pereira <humberto_pereira@live.com>
2023-07-13 13:19:09 +0000

    Add calculate_ingredients_stock method that calculate the value of the used ingredients in pizza manufacturing, and update send_dict_to_sheet method to fetch the headers from the first row of the worksheet, generating the data to be written in the same order as the headers

--- run.py
+++ run.py
@@ -23,8 +23,9 @@ class PizzaManagement:
         self.ingredients_kg_price = []
         self.pizza_sales = []
         # Dictionaries with key ingredients and respective weight in kg
-        self.ingredients_constant_dict = {
-            'flour': 0.35, 'east': 0.003, 'salt': 0.007, 'sugar': 0.010, 'tomato sauce': 0.5, 'cheese': 0.18}
+        
+        self.ingredients_constant_dict = { 'flour': 0.35, 'east': 0.003, 'salt': 0.007, 'olive oil': 0.010, 'sugar': 0.010, 'tomato sauce': 0.5, 'cheese': 0.18}
+
         self.margherita_dict = {'tomatoes': 0.03, 'oregano': 0.003}
 
         self.salami_dict = {'salami': 0.04, 'oregano': 0.003}
@@ -75,21 +76,21 @@ class PizzaManagement:
         # Calculate the daily average sold pizza
         avg_sold_pizzas = sum(sum(int(cell) for cell in row) for row in self.pizza_sales) / 6
         # Calculate the fixed cost per pizza
-        fixed_cost_per_pizza = self.fixed_costs / avg_sold_pizzas
+        fixed_cost_per_pizza = round(self.fixed_costs / avg_sold_pizzas, 2)
         # Calculate ingredients constant costs using the weight in kg from (ingredients_constant_dict) * (ingredients_kg_price_dict) ingredients kg price
         ingredients_constant_cost = 0
         for ingredient, weight in self.ingredients_constant_dict.items():
             if ingredient in ingredients_kg_price_dict:
-                ingredients_constant_cost += ingredients_kg_price_dict[ingredient] * weight
-
+                ingredients_constant_cost += round(ingredients_kg_price_dict[ingredient] * weight, 2)
+        # Calculate pizza cost
         pizzas_cost = {}
         for pizza_name, pizza_dict in pizzas.items():
             pizza_cost = 0
             for ingredient, weight in pizza_dict.items():
                 if ingredient in ingredients_kg_price_dict:
-                    pizza_cost += ingredients_kg_price_dict[ingredient] * weight
+                    pizza_cost += round(ingredients_kg_price_dict[ingredient] * weight, 2)
             pizza_cost += ingredients_constant_cost
-            pizzas_cost[pizza_name] = pizza_cost + fixed_cost_per_pizza
+            pizzas_cost[pizza_name] = round(pizza_cost + fixed_cost_per_pizza, 2)
         return pizzas_cost
 
     # def calculate_shopping_list_cost(self):
@@ -101,8 +102,7 @@ class PizzaManagement:
         # Call the function passing the 'dictionary of dictionaries'(self.pizzas) to calculate the cost of every pizza
         pizzas_cost = self.calculate_pizza_cost(self.pizzas)
         # Add the fixed cost per pizza and a 30% profit to the cost of each pizza and round the price to two decimal places
-        pizzas_price = {pizza_name: round(
-            pizza_cost + pizza_cost * 0.35, 1) for pizza_name, pizza_cost in pizzas_cost.items()}
+        pizzas_price = {pizza_name: round(pizza_cost + pizza_cost * 0.35, 2) for pizza_name, pizza_cost in pizzas_cost.items()}
         return pizzas_price
 
     # This method is reusable, it takes two arguments 'sheet_name' which is the name of the worksheet and 'data_dict' a dictionary containing the data to be written
@@ -110,20 +110,19 @@ class PizzaManagement:
         """
         Send the values of a dictionary to the specified worksheet
         """
-        # Generate the data to be written
-        data = [value for key, value in data_dict.items()]
-        # Get the worksheet
         worksheet = SHEET.worksheet(sheet_name)
+        headers = worksheet.row_values(1)
+        data = [data_dict.get(header, '')for header in headers]
         # Append the data to the next available row
         worksheet.append_row(data)
 
-    def calculate_profit(self, num_market_days_arg):
+    def calculate_profit(self):
         """
         Calculate the profit from the last market day or last 6 market days 'week'
         """
         worksheet = SHEET.worksheet('pizza sales')
         sales_data = worksheet.get_all_values()
-        num_market_days = sales_data[-num_market_days_arg]
+        num_market_days = sales_data[-1]
         total_cost = 0
         profit_per_pizza = {}
         pizzas_price = self.calculate_pizza_price()
@@ -131,23 +130,58 @@ class PizzaManagement:
         for pizza_name, quantity in zip(sales_data[0], num_market_days):
             if pizza_name in self.pizzas:
                 pizza_cost = self.calculate_pizza_cost({pizza_name: self.pizzas[pizza_name]})[pizza_name]
-                total_cost += pizza_cost * int(quantity)
-                revenue = pizzas_price[pizza_name] * int(quantity)
+                total_cost += round(pizza_cost * int(quantity), 2)
+                revenue = round(pizzas_price[pizza_name] * int(quantity), 2)
                 total_revenue += revenue
                 profit_per_pizza[pizza_name] = round(revenue - (pizza_cost * int(quantity)), 2)
         # profit contains profit of all pizzas and profit_per_pizza a dictionary with profit per pizza flavor
         profit = round(total_revenue - total_cost, 2)
-        return profit_per_pizza #, profit
-    # def calculate_ingredients_stock(self)
+        profit_per_pizza.update({'total profit': profit})
+        return profit_per_pizza
+
+    def calculate_ingredients_stock(self):
+        """
+        Calculate the ingredients used to manufacture all sold pizzas and subtract from existing stock values in the worksheet.
+        """
+        # Get the sales data from the 'pizza sales'
+        worksheet = SHEET.worksheet('pizza sales')
+        sales_data = worksheet.get_all_values()
+        # Get the data for the last market day
+        last_market_day = sales_data[-1]
+        ingredients_used = {}
+        # Calculate the quantity of ingredients used for each flavor
+        for pizza_name, quantity in zip(sales_data[0], last_market_day):
+            if pizza_name in self.pizzas:
+                for ingredient, weight in self.pizzas[pizza_name].items():
+                    if ingredient in ingredients_used:
+                        ingredients_used[ingredient] += round(weight * int(quantity), 2)
+                    else:
+                        ingredients_used[ingredient] = round(weight * int(quantity), 2)
+        # Calculate the quantity of constant ingredients used
+        total_pizzas_sold = sum([int(quantity) for quantity in last_market_day])
+        print(total_pizzas_sold)
+        for ingredient, weight in self.ingredients_constant_dict.items():
+            if ingredient in ingredients_used:
+                ingredients_used[ingredient] += round(weight * total_pizzas_sold, 2)
+            else:
+                ingredients_used[ingredient] = round(weight * total_pizzas_sold, 2)
+
+        return ingredients_used
+
+
 
     # def generate_shopping_list(self):
 
 
 test = PizzaManagement()
-pizzas_price = test.calculate_pizza_price()
-pprint(pizzas_price)
-pizza_profit = test.calculate_profit(1)
+pizzas_prices = test.calculate_pizza_price()
+pprint(f'pizzas price \n {pizzas_prices}')
+pizza_profit = test.calculate_profit()
 test.send_dict_to_sheet('profit', pizza_profit)
-print(pizza_profit)
-#test.send_dict_to_sheet('pizza selling price', pizzas_price)
-pprint(test.calculate_pizza_cost(test.pizzas))
+pprint(f'pizzas profit \n {pizza_profit}')
+ingredients_stock = test.calculate_ingredients_stock()
+test.send_dict_to_sheet('ingredients stock', ingredients_stock)
+pprint(f'ingredients stock \n {ingredients_stock}')
+test.send_dict_to_sheet('pizza selling price', pizzas_prices)
+print()
+pprint(f'pizza cost to manufacture \n {test.calculate_pizza_cost(test.pizzas)}')
